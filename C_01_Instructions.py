af5021e25bd6a7dbd64b9e8f2afe334af3e32a18
peterkhounn <peterkhoun@mmc.school.nz>
2024-05-09 09:10:52 +1200

    Basic instructions and simple yes / no function

---
+++ C_01_Instructions.py
@@ -0,0 +1,14 @@
+print("🎲🎲 Roll it 13 🎲🎲")
+
+# loop for testing purposes
+
+while True:
+    want_instructions = input("Do you want to read the instructions? ").lower()
+
+    # check users enter yes (y) or no (n)
+    if want_instructions == "yes" or want_instructions == "y":
+        print("you chose yes")
+    elif want_instructions == "no" or want_instructions == "n":
+        print("you chose no")
+    else:
+        print("you did not choose a valid response")
